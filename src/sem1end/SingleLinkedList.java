ea20e1e1197e6cb90f521cceba9b83f37961030c
venkata-bhagavan <890venkat@gmail.com>
2022-05-12 23:50:44 +0530

    SingleLinkedList my code (tested)

--- src/sem1end/SingleLinkedList.java
+++ src/sem1end/SingleLinkedList.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 
 class SingleLinkedList {
     public static Node head = null, tail = null, t, tra, ptr;
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         while (true) {
-            System.out.println("Enter your Choice: \n 1. append \t 2. insert \t 3. delete \t 4. display");
+            System.out.println("Enter your Choice: \n 1. append \t 2. insert \t 3. delete \t 4. display \t 5.Exit");
 
             int ch = sc.nextInt();
             switch (ch) {
@@ -17,133 +18,100 @@ class SingleLinkedList {
                 case 2 -> insert();
                 case 3 -> delete();
                 case 4 -> display();
-                default -> System.exit(0);
+                case 5 -> System.exit(0);
+                default -> print("Enter a valid choice");
             }
         }
     }
 
-
-    public static void append() {
-        Scanner sc = new Scanner(System.in);
-        if (tail != null) {
-            t = new Node();
-            int n;
-            System.out.println("Enter any value: ");
-            n = sc.nextInt();
-            t.data = n;
-            tail.lnk = t;
-            tail = t;
-            t.lnk = null;
-            tra = head;
-        } else {
-            t = new Node();
-
-            System.out.println("Enter any value: ");
-
-            t.data = sc.nextInt();
-            t.lnk = null;
-
-            head = t;
-            tail = t;
-            tra = head;
-        }
-    }
-
-    public static void insert() {
-        Scanner sc = new Scanner(System.in);
-        int target, count = 0;
-        if (head == null)
-
-            System.out.println("Insertion can not be done ");
-
-        else {
-            System.out.println("Enter target value: ");
-            target = sc.nextInt();
-            if (target == head.data) {
-                t = new Node();
-                int n;
-                System.out.println("Enter any value: ");
-                n = sc.nextInt();
-                t.data = n;
-                t.lnk = head;
-                head = t;
-            } else {
-                ptr = head.lnk;
-                while (ptr != null) {
-                    if (ptr.data == target) {
-                        count = 1;
-                        if (ptr.lnk != null) {
-                            t = new Node();
-                            int n;
-                            System.out.println("Enter any value: ");
-                            n = sc.nextInt();
-                            t.data = n;
-                            t.lnk = ptr.lnk;
-                            ptr.lnk = t;
-                        } else {
-                            t = new Node();
-                            System.out.println("Enter any value: ");
-                            t.data = sc.nextInt();
-                            //t.lnk = ptr.lnk;
-                            ptr.lnk = t;
-                            t.lnk = null;
-                            tail = t;
-                        }
-                    }
-                    ptr = ptr.lnk;
-                }
-                if (count == 0)
-                    System.out.println("Element Not Found ");
-            }
-        }
-    }
-
-    public static void delete() {
-        Scanner sc = new Scanner(System.in);
-        int target, count = 0;
+    private static void append() {
+        print("Enter the value : ");
+        t = new Node();
+        t.data = sc.nextInt();
         if (head == null) {
-            System.out.println("Deletion can not be done ");
+            head = t;
         } else {
-            System.out.println("Enter target value: ");
-            target = sc.nextInt();
-            if (target == head.data) {
-                ptr = head;
-                head = ptr.lnk;
-            } else {
-                ptr = head.lnk;
-                while (ptr != null) {
-                    if (ptr.data == target) {
-                        count = 1;
-                        if (ptr.lnk != null) {
-                            ptr.lnk = ptr.lnk.lnk;
-                        } else {
-                            tail = t;
-                        }
-                    }
-                    t = ptr;
-                    ptr = ptr.lnk;
+            tail.lnk = t;
+        }
+        tail = t;
+
+    }
+
+    protected static void insert() {
+        println("Enter the target value..");
+        int target = sc.nextInt();
+        if (target == head.data) {
+            t = new Node();
+            println("Enter the value");
+            t.data = sc.nextInt();
+            t.lnk = head;
+            head = t;
+        } else {
+            t = head;
+            int count = 0;
+            while (t != null && t.lnk != null) {
+                if (t.lnk.data == target) {
+                    count = 1;
+                    ptr = new Node();
+                    print("Enter the value : ");
+                    ptr.data = sc.nextInt();
+                    ptr.lnk = t.lnk;
+                    t.lnk = ptr;
+                    t = ptr.lnk;
                 }
-                if (count == 0)
-                    System.out.println("Element Not Found ");
+
+                t = t.lnk;
             }
+            if (count == 0) println("Target value " + target + " not found");
         }
     }
 
-
-    public static void display() {
-        //Scanner sc = new Scanner(System.in);
-        if (tra == null) {
-            System.out.println("No Elements");
+    protected static void delete() {
+        print("Enter the value to be deleted : ");
+        int target = sc.nextInt();
+        if (target == head.data) {
+            t = head.lnk;
+            head = t;
         } else {
-            tra = head;
-            while (tra != null) {
-                System.out.println(tra.data);
-                tra = tra.lnk;
-// System.out.println(tra);
+            int count = 0;
+            t = head;
+            while (t != null && t.lnk != null) {
+                if (t.lnk.data == target) {
+                    count += 1;
+                    if (t.lnk == tail) tail = t;
+                    t.lnk = t.lnk.lnk;
+                }
+                t = t.lnk;
             }
-            tra = head;
+            if (count == 0) println("Target value " + target + " not found..");
+        }
+
+    }
+
+    protected static void display() {
+        if (head == null) {
+            println("There are no elements in list..");
+        } else {
+            t = head;
+            println("The elements are...");
+            while (t != null) {
+                print(t.data + " ");
+                t = t.lnk;
+            }
+            println(" ");
         }
     }
+
+
+    private static <T> void print(T s) {
+        System.out.print(s);
+    }
+
+    private static <T> void println(T s) {
+        System.out.println(s);
+    }
+
+
 }
 
 
